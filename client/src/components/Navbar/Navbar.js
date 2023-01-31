be19a2ebf1ef6f66bbf3ca529d9f214c610627ea
Tooboi <japica123@gmail.com>
2023-01-31 16:33:17 -0500

    Cleaning Nav

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,50 +4,39 @@ import { Link } from 'react-router-dom';
 import Logo from './Logo';
 
 function Navbar() {
-    // Logout function
-    const logout = (event) => {
-        event.preventDefault();
-        Auth.logout();
-    };
-    // if logged in Nav
+  // Logout function
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  // if logged in Nav
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <div className="container justify-content-space-between">
           <Logo />
-          <div className='row flex-row'>
-            <Link to="/scores"><h3 className='mb-0 px-2'>Scores</h3></Link>
-            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
+          <div className="row flex-row">
+            <Link to="/scores">
+              <h3 className="mb-0 px-2">Scores</h3>
+            </Link>
+            <button className="btn btn-secondary mx-2" onClick={logout}>
+              Logout
+            </button>
           </div>
         </div>
-        
       );
-
-    //   <div className="container">
-    //       <Logo />
-    //       <ul className="flex-row">
-    //         <li className="mx-1">
-    //           <Link to="/scores">Scores</Link>
-    //         </li>
-    //         <li className="mx-1">
-    //           {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-    //           <a href="/" onClick={() => Auth.logout()}>
-    //             Logout
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-
     } else {
-        // Logged out Nav
+      // Logged out Nav
       return (
         <div className="container justify-content-space-between">
           <Logo />
-          <div className='row flex-row'>
-            <Link to="/signup"><h4 className='mb-0 px-2'>Signup</h4></Link>
-            <Link to="/login"><h4 className='mb-0 px-2'>Login</h4></Link>
+          <div className="row flex-row">
+            <Link to="/signup">
+              <h4 className="mb-0 px-2">Signup</h4>
+            </Link>
+            <Link to="/login">
+              <h4 className="mb-0 px-2">Login</h4>
+            </Link>
           </div>
         </div>
       );
