d432b37fee71aee440bb457267f6e0a05e10491a
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-31 13:55:38 -0500

    Added basic Question schema/model setup

--- server/models/Question.js
+++ server/models/Question.js
@@ -1,1 +1,22 @@
-
+const questionSchema = new mongoose.Schema({
+  type: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Type',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  choices: {
+    type: [String]
+  },
+  answer: {
+    type: String
+  },
+  feedback: {
+	type: String
+  }
+}, {
+  timestamps: true
+});
