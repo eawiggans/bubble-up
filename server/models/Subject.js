fa08a88b52f7e19432895c0202a5cb630996d979
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-31 14:41:07 -0500

    Added basic Scoreboard schema/model setup

--- server/models/Subject.js
+++ server/models/Subject.js
@@ -1,5 +1,14 @@
 const { Schema, model } = require('mongoose');
 
-const _ = model('', Schema);
+const subjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true
+});
+
+const _ = model('', subjectSchema);
 
 module.exports = _;
