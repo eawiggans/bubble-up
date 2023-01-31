01be537b68fce31225bca7159141fef69f9cecc9
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-31 13:38:59 -0500

    Added basic User schema/model setup

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  scores: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Score'
+  }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
