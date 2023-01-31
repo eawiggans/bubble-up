b16a6aa6c7c8fd6205ef6171eacbe6b494026548
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-31 15:26:09 -0500

    Added models to the index file and exported them

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,11 @@
+import Comment from "./Comment";
+import Difficulty from "./Difficulty";
+import Post from "./Post";
+import Question from "./Question";
+import QuestionType from "./QuestionType";
+import Quiz from "./Quiz";
+import Scoreboard from "./Scoreboard";
+import Subject from "./Subject";
+import User from "./User";
 
+module.exports = { Comment, Difficulty, Post, Question, QuestionType, Quiz, Scoreboard, Subject, User };
