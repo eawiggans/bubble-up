24fb335610f478ee055398e23a99d4c9027675d4
Emma Wiggans <ea.wiggans@gmail.com>
2023-02-01 13:05:22 -0500

    have complete layout

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { GET_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -7,6 +7,10 @@ import Auth from '../utils/auth';
 
 
 function Profile() {
+  // const { loading, data } = useQuery(GET_USER);
+
+  // const user = data?.user || [];
+
   // if (!Auth.loggedIn()) {
   //   return (
   //     <div className='container mt-5'>
@@ -17,37 +21,59 @@ function Profile() {
   //   )
   // } else {
   //   return (
-  //     code below will go here when hooked up!
+  //     code below will go here when login functionality is hooked up!
   //    this is written so far for viewing your own profile only
   //   )
   // }
-    return (
-        <div className='container mt-5'>
-          <div className='container profile-container flex-row  m-5'>
-            
-            <div className='card user-card flex-column col-3 me-5 p-3'>
+  return (
+    <div className='container mt-5'>
 
-                <div>
-                  <h3 className='profile-subheads'>User Info</h3>
-                  <div></div>
-                </div>
-                <div>
-                  <h3 className='profile-subheads'>Stats</h3>
-                  <div></div>
-                </div>
-            </div>
-            <div className='card activity-card flex-column col-8 p-3'>
-            <div className='row justify-content-center'><h2>Activities</h2></div>
-            <div className='row flex-column'>
-              <div className="row justify-content-center"><p>Ready to test yourself?</p></div>
-              <div className="row justify-content-center">
-            <Link to='/'><button className="btn btn-primary mt-2">Take a quiz!</button></Link>
+      <div className='container profile-container flex-row  m-5'>
+
+        <div className='card user-card flex-column col-3 me-5 p-3'>
+          <div className='pt-3'>
+            <h3 className='profile-subheads'>user</h3>
+            <div>name: user.firstName&nbsp;user.lastName</div>
+            <div>username: user.username</div>
+            <div className='row justify-content-end'><a href="LINK HERE">edit user info</a></div>
           </div>
-            </div>
-            </div>
+          
+          <div className='pt-3'>
+            <h3 className='profile-subheads'>stats</h3>
+            <div>last quiz: quiz name / quiz score</div>
+            <div>current average: average quiz score</div>
+            <div className='row justify-content-end'><Link to='/scoreboard'>view scoreboard</Link></div>
+          </div>
+
+          <div className='pt-3'>
+            <h3 className='profile-subheads'>posts</h3>
+            <div>recent posts: recent post title, recent post title, recent post title</div>
+            <div className='row justify-content-end'><Link to='/blog'>view blog</Link></div>
           </div>
         </div>
-      );
-    };
+
+        <div className='activity-cards flex-column col-8'>
+
+          <div className='card flex-column row justify-content-center p-3'>
+            
+              <div className="row justify-content-center">
+                <h2>Ready to test yourself?</h2>
+              </div>
+              <div className="row justify-content-center">
+                <Link to='/'><button className="btn btn-primary mt-2">Take a quiz!</button></Link>
+              </div>
+          </div>
+
+          <div className='card row justify-content-center mt-3 p-3'>
+            <h2>Your Last Quiz</h2>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
 
 export default Profile;
