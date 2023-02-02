6226b2f748230f4e12e48f5f419619e773488d7b
Tooboi <japica123@gmail.com>
2023-02-02 00:51:18 -0500

    Planning out blog page

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,1 +1,24 @@
 import React from 'react';
+
+
+const Blog = () => {
+
+    function QuestionList() {
+        const [state, dispatch]
+    }
+
+    return (
+        <div className=''>
+            <div className="container justify-content-center mt-5">
+                <div className="row justify-content-center flex-column mt-5">
+                    <h1>Top Questions</h1>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Blog;
