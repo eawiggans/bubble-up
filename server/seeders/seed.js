e0d7dbc6e65f7e6ff80b5335566d8167f9c7a20b
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-02-02 11:52:03 -0500

    Added seeds for User data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,16 +1,19 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Quiz, Prompt, Solution, Feedback, InterviewInfo } = require('../models');
 const userSeeds = require('./userSeeds.json');
 
 db.once('open', async () => {
   try {
     // Delete all existing User documents
-    await User.deleteMany({});
+    await User.collection.drop();
+    await Quiz.collection.drop();
+    await Prompt.collection.drop();
+    await Solution.collection.drop();
+    await Feedback.collection.drop();
+    await InterviewInfo.collection.drop();
 
     // Create new User documents
-    for (const seed of userSeeds) {
-      await User.create(seed);
-    }
+    await User.create(userSeeds);
 
   } catch (error) {
     console.log(error);
