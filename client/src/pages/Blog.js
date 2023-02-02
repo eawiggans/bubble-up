487edf2d182059a18bd2616ba3df2231d1106c88
Emma Wiggans <ea.wiggans@gmail.com>
2023-02-02 10:49:51 -0500

    removed myposts component

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,13 +1,11 @@
 import React from "react";
 import AllPosts from '../components/Blog/AllPosts';
 import SinglePost from '../components/Blog/SinglePost';
-import MyPosts from "../components/Blog/MyPosts";
 const Blog = () => {
     return (
       <div className="container justify-content-center mt-5">
         <AllPosts />
         {/* <SinglePost /> */}
-        {/* <MyPosts /> */}
       </div>
     );
   };
