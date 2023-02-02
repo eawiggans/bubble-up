58a18063cd7bc66f32e0425b6ecd328141940162
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-02-02 13:18:01 -0500

    Added seeds for Propmt data

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,6 +1,8 @@
 const db = require('../config/connection');
 const { User, Quiz, Prompt, Solution, Feedback, InterviewInfo } = require('../models');
+const { create } = require('../models/Solution');
 const userSeeds = require('./userSeeds.json');
+const promptSeeds = require('./promptSeeds.json')
 
 db.once('open', async () => {
   try {
@@ -14,8 +16,17 @@ db.once('open', async () => {
 
     // Create new User documents
     await User.create(userSeeds);
+    await Prompt.create(promptSeeds);
+
+    // const prompt = await Prompt.find({ prompt: /object oriented programming/i });
+
+    console.log(prompt);
 
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
+
+  console.log('all done!');
+  process.exit(0);
 });
