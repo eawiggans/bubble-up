09d10a8118b94a51e316201326e1983e5191fdcd
Tooboi <japica123@gmail.com>
2023-02-03 15:34:05 -0500

    Starting profile page

--- client/src/App.js
+++ client/src/App.js
@@ -84,15 +84,6 @@ function App() {
                 path="/prompt/:promptId" 
                 element={<Prompt />} 
               />
-            {/* <Route 
-                path="/success" 
-                element={<Success />} 
-              />
-              
-              <Route 
-                path="*" 
-                element={<NoMatch />} 
-              /> */}
             </Routes>
         </div>
       </Router>
