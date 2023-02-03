9a216ff3af40775b76dcf32217ea06f9f2de312b
Tooboi <japica123@gmail.com>
2023-02-03 11:09:34 -0500

    Auth changes

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -4,28 +4,24 @@
 import { Navigate, useParams } from 'react-router-dom';
 import Auth from '../utils/auth';
 
-import TextArea from '../components/TextArea/TextArea'
+import TextArea from '../components/TextArea/TextArea';
 // import Algorithm from '../components/Algorithm/Algorithm'
 
 const Quiz = () => {
-// ! ADD THE RETURN TO THIS AUTH
-//   if (!Auth.loggedIn()) {
-//     return <Navigate to="/" />;
-//   }
+  if (!Auth.loggedIn()) {
+    return <Navigate to="/" />;
+  } else
 
-    return (
-        <div className='full-dvh'>
-            <div className="container max-960 card justify-content-center force-v-center ">
-                <div className="row justify-content-center flex-column">
-                <TextArea />
-                {/* <Algorithm /> */}
-                </div>
-            </div>
+  return (
+    <div className="full-dvh">
+      <div className="container max-960 card justify-content-center force-v-center ">
+        <div className="row justify-content-center flex-column">
+          <TextArea />
+          {/* <Algorithm /> */}
         </div>
-
-    );
-
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default Quiz;
