72ff2a06bac573204f568630fffd287e0950c826
Tooboi <japica123@gmail.com>
2023-02-03 22:19:21 -0500

    Tabs

--- client/src/components/Blog/AllPosts.js
+++ client/src/components/Blog/AllPosts.js
@@ -7,13 +7,13 @@ const AllPosts = ({ prompts, title }) => {
   return (
     <div className="container col-8 flex-column m-5">
       <div className="row card flex-column justify-content-center">
-        <h2 className="row justify-content-center pt-3 mb-0">{title}</h2>
+        <h2 className="row justify-content-center pt-3 mb-3">{title}</h2>
         <div className="row flex-row justify-content-end">
-          <Link to="/writepost">
+          {/* <Link to="/writepost">
             <svg className="plus-svg-2" viewBox="0 0 448 512">
               <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
             </svg>
-          </Link>
+          </Link> */}
         </div>
       </div>
       <div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import Tabs, { TabPane } from 'rc-tabs';
 import { useMutation, useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 function Profile() {
-  const { loading, data } = useQuery(QUERY_ME);
+  const { username: userParam } = useParams();
 
-  const user = data?.user || [];
+  const { loading, error, data } = useQuery(QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  const user = data?.me || {};
   console.log(user);
+
+  const profileURL = `https://source.boringavatars.com/beam/150/${user._id}?colors=7dcfff,7aa2f7,bb9af7,b4f9f8,73daca,9ece6a`;
+
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
   if (!Auth.loggedIn()) {
     return (
@@ -21,12 +33,42 @@ function Profile() {
         </div>
       </div>
     );
+  }
+  if (loading) {
+    return <div>One sec...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
   } else {
     return (
-      <div className="container mt-5 justify-content-center">
-        <div className="row mt-5">
-            <img src="https://source.boringavatars.com/beam/150/justin?colors=7dcfff,7aa2f7,bb9af7,b4f9f8,73daca,9ece6a" alt="Profile Pic"></img>
+      <div className="container mt-5 justify-content-center flex-column">
+        <div className="row mt-5 justify-content-center">
+          <img src={profileURL} alt="Profile Pic" className="mb-4"></img>
         </div>
+        <div className="row align-items-center flex-column">
+          <h1 className="mb-5">
+            {user.firstName} {user.lastName}
+          </h1>
+        </div>
+        
+        <div className='container justify-content-center'>
+          <div onClick={() => toggleTab(1)} className={toggleState === 1 ? 'tabs row active-tabs px-4 mx-3 py-2' : 'row tabs px-4 mx-1 py-2'}>
+            Interviews
+          </div>
+          <div onClick={() => toggleTab(2)} className={toggleState === 2 ? 'tabs row active-tabs px-4 mx-3 py-2' : 'row tabs px-4 mx-1 py-2'}>
+            Solutions
+          </div>
+          <div onClick={() => toggleTab(3)} className={toggleState === 3 ? 'tabs row active-tabs px-4 mx-3 py-2' : 'row tabs px-4 mx-1 py-2'}>
+            Finished Prompts
+          </div>
+          <div onClick={() => toggleTab(4)} className={toggleState === 4 ? 'tabs row active-tabs px-4 mx-3 py-2' : 'row tabs px-4 mx-1 py-2'}>
+            Todo Prompts
+          </div>
+        </div><hr className='m-0'></hr>
+        <div className={toggleState === 1 ? 'content content-active' : 'content'}>Interviews</div>
+        <div className={toggleState === 2 ? 'content content-active' : 'content'}>Solutions</div>
+        <div className={toggleState === 3 ? 'content content-active' : 'content'}>Complected Prompts</div>
+        <div className={toggleState === 4 ? 'content content-active' : 'content'}>Prompts</div>
       </div>
     );
   }
