303539ee5e66064a73d84b84003fa7d6030c49ad
Emma Wiggans <ea.wiggans@gmail.com>
2023-02-05 18:11:59 -0500

    conditional rendering of interview header

--- client/src/components/Interviews/SingleInterview.js
+++ client/src/components/Interviews/SingleInterview.js
@@ -3,14 +3,27 @@ import { Link } from 'react-router-dom';
 
 const SingleInterview = ({ interviews }) => {
 
+  let company = ''
+    if (interviews.location) {
+        company += ` at ${interviews.location}`;
+    };
+    
+    let topic = ''
+    if (interviews.subject) {
+      topic += interviews.subject;
+    }
+
+    let job = ''
+    if (interviews.position) {
+      job += `for ${interviews.position}`
+    }
     return (
 
         <div className="row card interview-post-item flex-column mt-3 p-3">
             <div className="blog-post">
             <div>
-              <h2 className="row">{interviews.username}'s interview at {interviews.location}</h2>
+              <h2 className="row">{interviews.username}'s {topic} interview {company} {job}</h2>
             </div>
-                <div className='py-1'>{interviews.subject && <div><b>Subject:</b> {interviews.subject} </div>}</div>
                 <div className='py-1'><b>Prompt received:</b> {interviews.prompt}</div>
                 <div className='py-1'><b>Response:</b> {interviews.response}</div>
                 <div className='py-1'><b>Interviewer feedback:</b> {interviews.resFeedback}</div>
